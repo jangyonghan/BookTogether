2a8637b2f17eda2d56d289aaebacdff7079fca74
jangyonghan <wkddydgks1@gmail.com>
2025-01-21 23:53:46 +0900

    feat : bible api 추가

--- backend/src/routes/bible.js
+++ backend/src/routes/bible.js
@@ -7,8 +7,34 @@ const router = express.Router();
 router.get(
   "/bible",
   asyncHandler(async (req, res) => {
-    const bible = await Bible.find();
+    const count = parseInt(req.query.count, 10) || 50;
+    const bible = await Bible.aggregate([
+      { $sample: { size: count } }, // 랜덤으로 count 개수만큼 데이터를 가져옴
+    ]);
     res.json(bible);
+  })
+);
+
+router.post(
+  "/bible",
+  asyncHandler(async (req, res) => {
+    const { verse, text } = req.body;
+    const bible = await Bible.create({
+      verse,
+      text,
+    });
+    res.status(201).json(bible);
+  })
+);
+
+router.delete(
+  "/bible/:bibleId",
+  asyncHandler(async (req, res) => {
+    const bible = await Bible.findByIdAndDelete(req.params.bibleId);
+    if (!bible) {
+      return res.status(404).json({ massage: "bible not found" });
+    }
+    res.json({ message: "bible deleted" });
   })
 );
 
