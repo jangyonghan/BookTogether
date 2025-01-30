ba4aa31c6a97800453654bedc4fbcfde83728f17
jangyonghan <wkddydgks1@gmail.com>
2025-01-30 23:28:53 +0900

    feat : 폰트적용 및 예약현황 선택시 효과 수정

--- frontend/src/styles/GlobalStyle.tsx
+++ frontend/src/styles/GlobalStyle.tsx
@@ -8,13 +8,20 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+
+  @font-face {
+    font-family: "Ownglyph";
+    src: url("/fonts/Ownglyph.ttf") format("truetype");
+    font-weight: normal;
+    font-style: normal;
+  }
   
 
-  body {
 
+  body {
     margin: 0;
     padding: 0;
-    font-family: 'Arial', sans-serif; /* 기본 폰트 설정 */
+    font-family: 'Ownglyph', sans-serif; /* 기본 폰트 설정 */
     line-height: 1.5;
     background-color: #f8f9fa; /* 기본 배경색 */
     color: #212529; /* 기본 텍스트 색상 */
