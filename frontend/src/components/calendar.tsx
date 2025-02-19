dfea0d6794dd300f43563b50fa6c32dcd19b83fe
jangyonghan <wkddydgks1@gmail.com>
2025-02-20 01:29:06 +0900

    test

--- frontend/src/components/calendar.tsx
+++ frontend/src/components/calendar.tsx
@@ -1,20 +1,23 @@
 import { DayPicker } from "react-day-picker";
 import { ko } from "react-day-picker/locale";
-
+import "react-day-picker/style.css";
 import dayjs from "dayjs";
 import { useCalendarStore } from "../store/useCalendarStore";
 import styled from "styled-components";
 import { useEffect } from "react";
 
+const StyledDayPicker = styled.div`
+  .rdp-root {
+    --rdp-accent-color: #e0518b;
+  }
+`;
+
 export default function Calendar() {
   const { selectedDate, setSelectedDate } = useCalendarStore();
 
   useEffect(() => {
-    const today = dayjs().format("YYYY-MM-DD");
-    if (selectedDate !== today) {
-      setSelectedDate(today);
-    }
-  }, []);
+    console.log("렌더링된 selectedDate:", selectedDate);
+  }, [selectedDate]);
 
   const modifiersStyles = {
     selected: {
@@ -39,15 +42,17 @@ export default function Calendar() {
 
   return (
     <>
-      <DayPicker
-        locale={ko}
-        mode="single"
-        selected={dayjs(selectedDate).toDate()}
-        onSelect={handleSelect}
-        modifiers={modifiers}
-        modifiersStyles={modifiersStyles}
-        showOutsideDays
-      />
+      <StyledDayPicker>
+        <DayPicker
+          locale={ko}
+          mode="single"
+          selected={dayjs(selectedDate).toDate()}
+          onSelect={handleSelect}
+          modifiers={modifiers}
+          modifiersStyles={modifiersStyles}
+          showOutsideDays
+        />
+      </StyledDayPicker>
     </>
   );
 }
